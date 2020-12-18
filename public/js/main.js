4ca23441fcf636a422e46bf8cef2978371b3f581
Navezm <navez.martin@gmail.com>
2020-12-18 21:39:56 +0100

    projet fini

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ let divTask = document.getElementById("divTask");
 let btnAdd = document.getElementById("btnAdd");
 let inputAdd = document.getElementById("inputAdd");
 let btnSort = document.getElementsByClassName("btnSort");
+let deleteDiv = document.getElementById("deleteDiv");
 
 // Déclaration élément création
 let newTask;
@@ -18,7 +19,9 @@ let btnCheck;
 let btnEdit;
 let btnDelete;
 let btnSave;
+let childI;
 let inputChange;
+let divDelete = [];
 
 // // Event création de tâches
 btnAdd.addEventListener("click", function(){
@@ -31,8 +34,12 @@ btnAdd.addEventListener("click", function(){
     div1Task = document.createElement("div");
     div1Task.setAttribute("class", "d-flex");
     div1Task.classList.add("col-4", "align-items-center");
-    span = document.createElement("span");
-    span.innerHTML = inputAdd.value;
+    span = document.createElement("input");
+    span.setAttribute("disabled", "");
+    span.setAttribute("class", "inputChange");
+    span.classList.add("bg-transparent");
+    span.value = inputAdd.value;
+    span.style.border = "none";
     div1Task.appendChild(span);
 
     // Boutons tâches
@@ -52,6 +59,7 @@ btnAdd.addEventListener("click", function(){
             button.classList.add("text-warning", "btnEdit");
             logo = document.createElement("i");
             logo.setAttribute("class", "fas fa-edit");
+            logo.classList.add("arrayI");
             button.appendChild(logo);
         } else if(i == 2){
             button.classList.add("text-danger", "btnDelete");
@@ -70,6 +78,7 @@ btnAdd.addEventListener("click", function(){
     btnCheck = document.getElementsByClassName("btnCheck");
     btnEdit = document.getElementsByClassName("btnEdit"); 
     btnDelete = document.getElementsByClassName("btnDelete");
+    childI = document.getElementsByClassName("arrayI");
 
     // // Event bouttons tâche
     // Event Check
@@ -81,16 +90,37 @@ btnAdd.addEventListener("click", function(){
     };
 
     // Event delete
-    for (let i = 0; i < divTaskArray.length; i++) {
-        btnDelete[i].addEventListener("click", function(e){
-            divTaskArray[i]
+    for (let i = 0; i < btnDelete.length; i++) {
+        btnDelete[i].addEventListener("click", function(){
+            divTaskArray[i].classList.remove("d-flex");
+            divTaskArray[i].classList.add("d-none");
+            divTaskArray[i].classList.remove("bg-light");
+            divTaskArray[i].classList.remove("bg-success");
+            divDelete.push(divTaskArray[i]);
         });
     };
 
-    // Event modify
-    for (let i = 0; i < divTaskArray.length; i++) {
-        btnEdit[i].addEventListener("click", function(){
+    deleteDiv.addEventListener("click", function(){
+        for (let i = 0; i < (divDelete.length + 1); i++) {
+            divDelete[i].remove();
+        };
+    });
 
+    // Event modify
+       for (let i = 0; i < btnEdit.length; i++) {
+        btnEdit[i].addEventListener("click", function(){
+            spanInput[i].removeAttribute("disabled");
+            childI[i].classList.remove("fa-edit");
+            childI[i].classList.add("fa-save");
+        });
+    };
+
+    for (let i = 0; i < spanInput.length; i++) {
+        spanInput[i].addEventListener("blur", function(){
+            spanInput[i].setAttribute("disabled", "");
+            childI[i].classList.remove("fa-save");
+            childI[i].classList.add("fa-edit");
+            
         });
     };
 
@@ -134,3 +164,4 @@ btnAdd.addEventListener("click", function(){
 
 // // Déclaration & stockage des éléments tâches
 let divTaskArray = document.getElementsByClassName("divTaskArray");
+let spanInput = document.getElementsByClassName("inputChange");
